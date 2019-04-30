fa61df2e0c97b2f5196ae94ff8b0b2449b623d1c
Nahuel <salemnahuel@gmail.com>
2019-04-30 20:20:26 +0200

    Update modelFitQLearner.m

--- modelFitQLearner.m
+++ modelFitQLearner.m
@@ -15,6 +15,7 @@ for imodel = 1:numel(modelsinfo)
     x0 = modelsinfo{imodel}.x0;
     for isub = 1:size(Choices,2)
         [parameters{imodel}(isub,:),LPP(isub,imodel),report(isub,imodel),gradient{isub,imodel},hessian{isub,imodel}]=fmincon(@(x) GetModelLL_QLearner(x,modelsinfo{imodel},Choices(:,isub),Reward(:,isub),1),x0,[],[],[],[],lb,ub,[],options);
+        nfpm = numel(modelsinfo{irecmodel}.paramnames);
         this_ll =  GetModelLL_QLearner(parameters{imodel}(isub,:),modelsinfo{imodel},Choices(:,isub),Reward(:,isub),0);
         bic(isim, isub,igenmodel,irecmodel)=-2*-this_ll+nfpm*log(ntrials);
         aic(isim, isub,igenmodel,irecmodel)=-2*-this_ll+nfpm; 

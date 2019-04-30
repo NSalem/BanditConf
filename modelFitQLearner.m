65e13f41bf148dee9c8188f7ba4fea408ab4a4b5
Nahuel <salemnahuel@gmail.com>
2019-04-30 20:23:26 +0200

    Update modelFitQLearner.m

--- modelFitQLearner.m
+++ modelFitQLearner.m
@@ -8,6 +8,8 @@ Choices(Choices==3) = 2;
 
 loadModelsInfo; 
 
+ntrials = size(Choices,1);
+
 options = optimset('Algorithm', 'interior-point', 'Display', 'final', 'MaxIter', 10000); % These increase the number of iterations to ensure the convergence
 for imodel = 1:numel(modelsinfo) 
     lb = modelsinfo{imodel}.lb;
@@ -15,10 +17,10 @@ for imodel = 1:numel(modelsinfo)
     x0 = modelsinfo{imodel}.x0;
     for isub = 1:size(Choices,2)
         [parameters{imodel}(isub,:),LPP(isub,imodel),report(isub,imodel),gradient{isub,imodel},hessian{isub,imodel}]=fmincon(@(x) GetModelLL_QLearner(x,modelsinfo{imodel},Choices(:,isub),Reward(:,isub),1),x0,[],[],[],[],lb,ub,[],options);
-        nfpm = numel(modelsinfo{irecmodel}.paramnames);
+        nfpm = numel(modelsinfo{imodel}.paramnames);
         this_ll =  GetModelLL_QLearner(parameters{imodel}(isub,:),modelsinfo{imodel},Choices(:,isub),Reward(:,isub),0);
-        bic(isim, isub,igenmodel,irecmodel)=-2*-this_ll+nfpm*log(ntrials);
-        aic(isim, isub,igenmodel,irecmodel)=-2*-this_ll+nfpm; 
+        bic(isub,imodel)=-2*-this_ll+nfpm*log(ntrials);
+        aic(isub, imodel)=-2*-this_ll+nfpm; 
     end
 end
 
